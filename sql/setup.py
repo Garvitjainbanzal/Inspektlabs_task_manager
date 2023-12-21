b5a85b43238b880c5aefcbe2420b8e63a06b7cc4
Garvit <i-garvit.banzal@foundit.ai>
2023-12-21 15:33:04 +0530

    updated database uri

--- sql/setup.py
+++ sql/setup.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('mysql+pymysql://root:9899@localhost/task_manager')
+engine = create_engine('mysql+mysqlconnector://root:password@13.233.73.21:8081/task_manager')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
